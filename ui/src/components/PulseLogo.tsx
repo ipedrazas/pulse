676ecf4ea6e6aa77fb3bc4603bc47f31f627d598
Ivan Pedrazas <ipedrazas@gmail.com>
2026-03-06 17:30:06 +0000

    chore: fix fmt issues
    Signed-off-by: Ivan Pedrazas <ipedrazas@gmail.com>

--- ui/src/components/PulseLogo.tsx
+++ ui/src/components/PulseLogo.tsx
@@ -1,10 +1,6 @@
-export function PulseLogo({ className = "h-7 w-7" }: { className?: string }) {
+export function PulseLogo({ className = 'h-7 w-7' }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 32 32"
-      className={className}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className={className}>
       <rect width="32" height="32" rx="6" fill="#1a2332" />
       <polyline
         points="3,18 9,18 12,8 16,24 20,12 23,18 29,18"
